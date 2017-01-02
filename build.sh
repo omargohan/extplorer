92f94a1c00ffb076485f7506543f9d94c4e326be
Morgan Blackthorne <morgan@windsofstorm.net>
2017-01-02 14:36:33 -0800

    Fix build script

--- build.sh
+++ build.sh
@@ -8,14 +8,14 @@ fi
 DATE=$(date +%Y%m%d.%H%M)
 
 echo "Building docker-extplorer:trusty.$DATE"
-docker build $CACHE -t="stormerider/docker-extplorer:trusty.$DATE" . & \
-echo "" & \
-echo "Pushing docker-extplorer:trusty.$DATE" & \
+docker build $CACHE -t="stormerider/docker-extplorer:trusty.$DATE" . && \
+echo "" && \
+echo "Pushing docker-extplorer:trusty.$DATE" && \
 docker push stormerider/docker-extplorer:trusty.$DATE
 
 echo ""
 echo "Building docker-extplorer:latest"
-docker build $CACHE -t="stormerider/docker-extplorer:latest" . & \
-echo "" & \
-echo "Pushing docker-extplorer:latest" & \
+docker build $CACHE -t="stormerider/docker-extplorer:latest" . && \
+echo "" && \
+echo "Pushing docker-extplorer:latest" && \
 docker push stormerider/docker-extplorer:latest
