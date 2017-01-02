ad7379cd6b926df9ba18dbd4380930bc9bbccc10
Morgan Blackthorne <morgan@windsofstorm.net>
2017-01-02 14:31:59 -0800

    Update config and prep for Bamboo builds

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+if [ "x$1" == "x" ]; then
+  CACHE="--no-cache"
+else
+  CACHE=""
+fi
+
 DATE=$(date +%Y%m%d.%H%M)
+
 echo "Building docker-extplorer:trusty.$DATE"
-docker build --no-cache -t="stormerider/docker-extplorer:trusty.$DATE" .
+docker build $CACHE -t="stormerider/docker-extplorer:trusty.$DATE" . & \
+echo "" & \
+echo "Pushing docker-extplorer:trusty.$DATE" & \
+docker push stormerider/docker-extplorer:trusty.$DATE
+
 echo ""
 echo "Building docker-extplorer:latest"
-docker build -t="stormerider/docker-extplorer:latest" .
-echo ""
-echo "Pushing docker-extplorer:trusty.$DATE"
-docker push stormerider/docker-extplorer:trusty.$DATE
-echo ""
-echo "Pushing docker-extplorer:latest"
+docker build $CACHE -t="stormerider/docker-extplorer:latest" . & \
+echo "" & \
+echo "Pushing docker-extplorer:latest" & \
 docker push stormerider/docker-extplorer:latest
-echo ""
